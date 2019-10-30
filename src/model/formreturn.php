68d91a25946e7817910deee68f6d87d74d52ab4e
valasulth <romain-culleron_student2019@wilder.school>
2019-10-30 19:31:25 +0100

    add some color

--- src/model/formreturn.php
+++ src/model/formreturn.php
@@ -12,16 +12,21 @@ $data = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html>
 <header>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </header>
 
 <body>
-        <?php
+  <div class="form-return">
+
+  <?php
         foreach($data as $key => $value)
         {
             echo '<div class="container-fluid">';
             echo '<div class="row">';
-            echo '<div class="card">';
+            echo '<div class= "card col-sm-12 col-md-6 col-lg-3">';
+            echo '<div class="test"';
             echo '<div class="card-body">';
             echo '<p>' . $value["Movie_name"] . '</p>';
             echo '<p>' . $value["Main_character"] . '</p>';
@@ -31,13 +36,10 @@ $data = $statement->fetchAll(PDO::FETCH_ASSOC);
             echo '</div>';
             echo '</div>';
             echo '</div>'; 
-          
+            echo '</div>';
         }
         ?>
-        </div>
-     </div>
   </div>
-</div> 
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
