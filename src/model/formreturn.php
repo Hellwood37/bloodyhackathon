267ee7a9cdd51267585bf76de4fdb32c9e7548be
valasulth <romain-culleron_student2019@wilder.school>
2019-10-30 17:11:13 +0100

    add some bootstrap still bug

--- src/model/formreturn.php
+++ src/model/formreturn.php
@@ -9,28 +9,26 @@ $statement->execute();
 $data = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<html>
+<header>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</header>
 
- <div class="container-fluid">
-  <div class="row">
-     <div class="card">
-        <div class="card-body">
+<body>
         <?php
         foreach($data as $key => $value)
         {
           foreach($value as $movie)
           {
-            echo '<h3>'; 
-            echo $movie;
-            echo '</h3>';
-            echo '<p>';
-            echo $movie;
-            echo '</p>';
-            echo '<p>';
-            echo $movie;
-            echo '</p>';
-            echo '<p>';
-            echo $movie;
-            echo '</p>';
+            echo '<div class="container-fluid">';
+            echo '<div class="row">';
+            echo '<div class="card">';
+            echo '<div class="card-body">';
+            echo '<p>' . $movie . '</p>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>'; 
           }
         }
         ?>
@@ -38,5 +36,11 @@ $data = $statement->fetchAll(PDO::FETCH_ASSOC);
      </div>
   </div>
 </div> 
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
+
 
 
