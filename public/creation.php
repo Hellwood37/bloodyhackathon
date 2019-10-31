083642b4a48cff08157b7ad64a23128c73a4c8fb
Alexandre Verbreugh <alverbreugh@gmail.com>
2019-10-31 14:34:01 +0100

    mise ajour

--- public/creation.php
+++ public/creation.php
@@ -9,6 +9,8 @@ require_once 'form.php';
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
   <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+  <link rel="shortcut icon" href="https://nsm09.casimages.com/img/2019/10/31//19103102235924921116486658.png"/>
+
   <link rel="stylesheet" href="creation.css">
   <title>Your movie</title>
 </head>
